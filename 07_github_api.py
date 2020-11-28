4091f89f4ca4d5b51814276cefada2b24dd24717
AntonioCh <antoniochavezcampos94@gmail.com>
2020-11-28 13:19:15 -0600

    Added API

--- 07_github_api.py
+++ 07_github_api.py
@@ -3,17 +3,30 @@ import requests
 #CONSTANTS
 base_url = 'https://api.github.com/'
 
-respose = requests.get(base_url)
+#respose = requests.get(base_url)
 
 #FUNCTION - construyendo la variable para consulta info del usuario
 def get_github_user(username):
     url = f'{base_url}users/{username}'
-    respose = requests.get(url)
-    if respose.status_code == 200:
-        return  respose.json() #para mandar traer función JSON
+    response = requests.get(url)
+    if response.status_code == 200:
+        return  response.json() #para mandar traer función JSON
     return None
+
+def download_github_user_avatar(avatar_url, username):
+    response = requests.get(avatar_url)
+    if response.status_code ==200:
+        #download a file from internet
+        response_content = response.content #objeto binario
+        filename = f'tmp/{username}.png'
+        with open(filename, 'wb') as image:#abreme un archivo que va a escribir en b(binario)cierto contenido
+            image.write(response_content)
+            return filename
+    return None
+
 
 username = input('Give me a github username:\t')
 user = get_github_user(username)
-print(user)
+filename = download_github_user_avatar(user['avatar_url'], username)
+print(filename)
 
---
+++ 08_hm_githubuser_followers.py
@@ -0,0 +1,6 @@
+#Ocupando conocimientos de python y requests
+
+#PUNTOS A REALIZAR
+#Crear un bot para consultar los followers de un usuario de github
+#Hecha dicha consulta, descargar el avatar de cada follower
+
