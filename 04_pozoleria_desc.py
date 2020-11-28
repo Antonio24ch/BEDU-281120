bda09ab00cb28b244e6004b96070d4f237e183c8
AntonioCh <antoniochavezcampos94@gmail.com>
2020-11-28 10:50:28 -0600

    Homework

---
+++ 04_pozoleria_desc.py
@@ -0,0 +1,7 @@
+#Aplicar conocimientos de la función MAP en python
+
+#PUNTOS A REALIZAR:
+#Agregar descuento aleatorio entre 5, 10 y 15 % a los precios 
+#Imprimir menu sin descuento
+#Imprimir menu con descuento
+precios_en_menu = [25,87,32,66,25,10,55,88,100]
---
+++ 05_numero_magicos.py
@@ -0,0 +1,7 @@
+#Aplicar conocimientos de la función MAP en python
+
+#PUNTOS A REALIZAR:
+#Solicitar al usuario un numero menor que 10 y mayor que 0
+#Convertir los num en enteros
+#Utilizando el numero del usuario, aplicar a la lista una multiplicacion por el numero del usuario
+#Coonvertir numeros pares a True
